858b03b9ddfaf77abb0e285716a4c47dd483c1c8
larrychen <lianggg26@gmail.com>
2019-11-20 18:33:57 +0800

    feat: init java code

---
+++ server/src/main/java/com/thmnet/xlab/hrflex/HrflexApplication.java
@@ -0,0 +1,13 @@
+package com.thmnet.xlab.hrflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrflexApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/thmnet/xlab/hrflex/HrflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thmnet.xlab.hrflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
